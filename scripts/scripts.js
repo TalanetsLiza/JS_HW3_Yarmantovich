3663d1840afdd96d5efb29f8710c6f32a05b8786
TalanetsLiza <yarmantovich1999@mail.ru>
2023-03-05 21:11:56 +0300

    add: complete task 6

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -61,3 +61,15 @@
     }    
 
 }
+
+{
+    // Task 6
+
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            console.log(`${i} * ${j} = ${i * j}`);
+        }
+        console.log("")
+    }
+    
+}
